49f341dffa6cf016a355d69b9b783b05a12341d3
komsak007 <lert_zodiac@hotmail.com>
2021-10-10 22:30:50 +0700

    First commit Task list

---
+++ src/components/Button.vue
@@ -0,0 +1,17 @@
+<template>
+  <button @click="onClick()" class="btn" :style="{ background: color }">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: { text: String, color: String },
+  methods: {
+    onclick() {
+      confirm("aaaa");
+    },
+  },
+};
+</script>
